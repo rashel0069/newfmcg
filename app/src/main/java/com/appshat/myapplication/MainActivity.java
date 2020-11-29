d6b53ef4f18ad1d83fc67820baec0a32b8482268
zinnia <zinnia@digitalistic.net>
2020-11-29 19:06:48 +0600

    29/11/2020

---
+++ app/src/main/java/com/appshat/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.appshat.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/appshat/myapplication/fragment/Information_Fragment.java
@@ -0,0 +1,29 @@
+package com.appshat.myapplication.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.appshat.myapplication.R;
+public class Information_Fragment extends Fragment {
+    Button saveBtn;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
+            Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_information_, container, false);
+
+       saveBtn = view.findViewById(R.id.saved_id);
+     return  view;
+
+    }
+}
