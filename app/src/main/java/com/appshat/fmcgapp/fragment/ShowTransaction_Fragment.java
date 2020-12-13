c1a3824b75fc9b7fe8b58aabcc14f1f6a148d2e4
zinnia <zinnia@digitalistic.net>
2020-12-13 13:30:55 +0600

    13-12-2020 1:30

--- app/src/main/java/com/appshat/fmcgapp/fragment/ShowTransaction_Fragment.java
+++ app/src/main/java/com/appshat/fmcgapp/fragment/ShowTransaction_Fragment.java
@@ -3,21 +3,33 @@ package com.appshat.fmcgapp.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.appshat.fmcgapp.R;
 
 
 public class ShowTransaction_Fragment extends Fragment {
+    EditText searchmbl;
+    Button searchBtn, showallBtn;
+    RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_show_transaction_, container, false);
+
+        searchmbl = view.findViewById(R.id.smblET_id);
+        searchBtn = view.findViewById(R.id.searchBtn_id);
+        showallBtn = view.findViewById(R.id.showallBtn_id);
+        recyclerView = view.findViewById(R.id.showallBtn_id);
+
         return view;
 
     }
